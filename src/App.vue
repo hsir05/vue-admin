c185fd070ffc7764c90ce68e3c4117d1c96c6880
hdq <haqsir@163.com>
2020-03-11 22:17:37 +0800

    国际化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
-    <router-view />
+    <a-locale-provider :locale="zh_CN">
+      <router-view />
+    </a-locale-provider>
   </div>
 </template>
+<script>
+import zh_CN from "ant-design-vue/lib/locale-provider/zh_CN";
 
+import moment from "moment";
+import "moment/locale/zh-cn";
+
+moment.locale("en");
+export default {
+  data() {
+    return {
+      zh_CN
+    };
+  }
+};
+</script>
 <style lang="scss">
 #app {
   height: 100vh;
