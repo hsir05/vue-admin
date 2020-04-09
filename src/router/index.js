dc88b1211697b5b34f97b30d99a207bf4712b68c
hdq <haqsir@163.com>
2020-04-09 09:23:25 +0800

    添加权限，缓存

--- src/router/index.js
+++ src/router/index.js
@@ -23,50 +23,86 @@ const routes = [
         path: "/",
         icon: "user",
         name: "Home",
+        meta: {
+            keepAlive: true,
+            auth: ['visitor', 'administrator']
+        },
         component: () => import("../views/Home.vue")
       },
       {
         path: "/about",
         name: "About",
         icon: "video-camera",
+          meta: {
+              keepAlive: true,
+              auth: ['visitor', 'administrator']
+          },
         component: () => import("../views/About.vue")
       },
       {
         path: "/list",
         name: "List",
         icon: "bars",
+          meta: {
+              keepAlive: true,
+              auth: ['visitor', 'administrator']
+          },
         component: () => import("../views/list/List.vue")
       },
       {
         path: "/echarts",
         name: "echarts",
         icon: "bars",
+          meta: {
+              keepAlive: true,
+              auth: ['visitor', 'administrator']
+          },
         component: () => import("../views/echarts/Echarts.vue")
       },
       {
         path: "/test",
         name: "Test",
+          meta: {
+              keepAlive: true,
+              auth: ['visitor', 'administrator']
+          },
         icon: "setting",
         component: () => import("../views/test/Test.vue"),
         children: [
           {
             path: "/test/children",
             name: "Children",
+            meta: {
+                keepAlive: true,
+                auth: ['visitor', 'administrator']
+            },
             component: () => import("../views/test/Children.vue")
           },
           {
             path: "/form",
+            meta: {
+                keepAlive: true,
+                auth: ['visitor', 'administrator']
+            },
             name: "Form",
             component: () => import("../views/form/Basic.vue")
           },
           {
             path: "/ueditor",
+            meta: {
+                keepAlive: true,
+                auth: ['visitor', 'administrator']
+            },
             name: "Ueditor",
             component: () => import("../views/form/Ueditor.vue")
           },
           {
             path: "/test/list",
             name: "List2",
+            meta: {
+                keepAlive: true,
+                auth: ['visitor', 'administrator']
+            },
             component: () => import("../views/list/List.vue")
           },
           {
